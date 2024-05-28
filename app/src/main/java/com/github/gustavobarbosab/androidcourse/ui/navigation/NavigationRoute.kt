feb49fbc1a1cb91ec8a2f19eef4df2a6d6779d83
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2024-05-28 11:33:01 -0300

    moving routes

--- app/src/main/java/com/github/gustavobarbosab/androidcourse/ui/navigation/NavigationRoute.kt
+++
@@ -1,6 +0,0 @@
-package com.github.gustavobarbosab.androidcourse.ui.navigation
-
-sealed class NavigationRoute(val route: String) {
-    data object Login: NavigationRoute("LOGIN")
-    data object Home: NavigationRoute("HOME")
-}
