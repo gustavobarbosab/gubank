07ec8865c23aa2f632958ce29f08422ac130aa16
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2024-05-29 13:43:09 -0300

    removing unused classes

--- app/src/main/java/com/github/gustavobarbosab/androidcourse/ui/screen/login/model/InputValidationState.kt
+++
@@ -1,6 +0,0 @@
-package com.github.gustavobarbosab.androidcourse.ui.screen.login.model
-
-sealed class InputValidationState {
-    data object ValidField : InputValidationState()
-    class InvalidField(val feedbackResource: LoginFeedbackResource) : InputValidationState()
-}
