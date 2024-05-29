05f3630e131a5cce5ab77d6210da5d4362a93aa4
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2024-05-29 13:37:12 -0300

    creating error text field

---
+++ app/src/main/java/com/github/gustavobarbosab/androidcourse/ui/common/widgets/ErrorTextField.kt
@@ -0,0 +1,31 @@
+package com.github.gustavobarbosab.androidcourse.ui.common.widgets
+
+import androidx.annotation.StringRes
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.github.gustavobarbosab.androidcourse.ui.common.theme.errorLight
+
+@Composable
+fun ErrorTextField(
+    modifier: Modifier = Modifier,
+    inputValidation: InputValidationState,
+) {
+    when (inputValidation) {
+        is InputValidationState.InvalidField -> {
+            Text(
+                modifier = modifier,
+                text = stringResource(id = inputValidation.feedbackResource),
+                color = errorLight
+            )
+        }
+
+        InputValidationState.ValidField -> Unit
+    }
+}
+
+sealed class InputValidationState {
+    data object ValidField : InputValidationState()
+    class InvalidField(@StringRes val feedbackResource: Int) : InputValidationState()
+}
