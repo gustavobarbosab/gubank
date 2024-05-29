fd5d9eec60999f3b4f1b5a56ab288e323f909901
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2024-05-29 13:43:21 -0300

    adding methods to RegisterFlowViewModel

--- app/src/main/java/com/github/gustavobarbosab/androidcourse/ui/screen/register/RegisterFlowViewModel.kt
+++ app/src/main/java/com/github/gustavobarbosab/androidcourse/ui/screen/register/RegisterFlowViewModel.kt
@@ -13,7 +13,50 @@ class RegisterFlowViewModel : ViewModel() {
     val myTextState
         get() = _myTextState.asStateFlow()
 
+    private var _userName = MutableStateFlow("")
+    val userName
+        get() = _userName.asStateFlow()
+
+    private var _userDocument = MutableStateFlow("")
+    val userDocument
+        get() = _userDocument.asStateFlow()
+
+    private var _userBirthday = MutableStateFlow("")
+    val userBirthday
+        get() = _userBirthday.asStateFlow()
+
+    private var _userAddress = MutableStateFlow("")
+    val userAddress
+        get() = _userAddress.asStateFlow()
+
     fun updateShared(sharedString: String) {
         _myTextState.update { "$it $sharedString" }
+    }
+
+    fun saveUserName(userName: String) {
+        _userName.update { userName }
+    }
+
+    fun saveDocument(document: String) {
+        _userDocument.update { document }
+    }
+
+    fun saveBirthday(birthday: String) {
+        _userBirthday.update { birthday }
+    }
+
+    fun saveAddress(
+        cep: String,
+        street: String?,
+        district: String?,
+        city: String?,
+        number: String?
+    ) {
+        _userAddress.update {
+            val stringBuilder = StringBuilder()
+                .append(street)
+                .append(number)
+            "$street, $number - $district. $city"
+        }
     }
 }
