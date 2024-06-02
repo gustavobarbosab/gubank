cace8b1a242649c1767622e0f8018608058acdac
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2024-06-02 16:03:26 -0300

    creating shared repository

---
+++ app/src/main/java/com/github/gustavobarbosab/androidcourse/ui/screen/register/data/RegisterFlowRepositoryImpl.kt
@@ -0,0 +1,14 @@
+package com.github.gustavobarbosab.androidcourse.ui.screen.register.data
+
+import java.util.Calendar
+
+class RegisterFlowRepositoryImpl : RegisterFlowRepository {
+    override var name = ""
+    override var birthday: Calendar = Calendar.getInstance()
+    override var document = ""
+    override var street = ""
+    override var district = ""
+    override var number = ""
+    override var city = ""
+    override var cep = ""
+}
