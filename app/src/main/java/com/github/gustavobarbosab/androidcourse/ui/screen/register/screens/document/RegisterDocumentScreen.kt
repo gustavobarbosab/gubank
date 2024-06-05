43bbd20a78d6ad81682c50b497cf7ca0db0130e0
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2024-06-05 15:01:54 -0300

    creating the register document

--- app/src/main/java/com/github/gustavobarbosab/androidcourse/ui/screen/register/screens/document/RegisterDocumentScreen.kt
+++ app/src/main/java/com/github/gustavobarbosab/androidcourse/ui/screen/register/screens/document/RegisterDocumentScreen.kt
@@ -2,34 +2,86 @@ package com.github.gustavobarbosab.androidcourse.ui.screen.register.screens.docu
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.github.gustavobarbosab.androidcourse.R
+import com.github.gustavobarbosab.androidcourse.ui.common.components.ErrorTextField
+import com.github.gustavobarbosab.androidcourse.ui.common.components.IsolationColumn
 import com.github.gustavobarbosab.androidcourse.ui.common.components.PrimaryButton
+import com.github.gustavobarbosab.androidcourse.ui.common.components.ToolbarIcon
+import com.github.gustavobarbosab.androidcourse.ui.common.size.fontSizeMedium
+import com.github.gustavobarbosab.androidcourse.ui.common.size.paddingSmall
+import com.github.gustavobarbosab.androidcourse.ui.common.theme.secondaryLight
 import com.github.gustavobarbosab.androidcourse.ui.screen.register.RegisterFlowViewModel
+import com.github.gustavobarbosab.androidcourse.ui.screen.register.common.extension.registerToolbarSetup
+import com.github.gustavobarbosab.androidcourse.ui.screen.register.common.model.RegisterScreenState
+import com.github.gustavobarbosab.androidcourse.ui.screen.register.screens.name.RegisterNameViewModel
 
 @Composable
 fun RegisterDocumentScreen(
-    registerFlowViewModel: RegisterFlowViewModel,
+    screenState: RegisterScreenState = RegisterScreenState(
+        stringResource(R.string.register_toolbar_name),
+        stringResource(R.string.register_name_header),
+        stringResource(R.string.register_name_hint)
+    ),
+    sharedViewModel: RegisterFlowViewModel,
+    viewModel: RegisterNameViewModel,
     navigateToAddressScreen: () -> Unit
 ) {
-    val textState = registerFlowViewModel.myTextState.collectAsState()
-    LaunchedEffect(Unit) {
-        registerFlowViewModel.updateShared("Documento")
-    }
+    val inputState by viewModel.userName.collectAsState()
+
+    sharedViewModel.registerToolbarSetup(
+        screenState.toolbarTitle,
+        ToolbarIcon.Back
+    )
+
     Column(
         Modifier
             .fillMaxSize()
             .background(Color.White)
     ) {
-        Text(text = textState.value)
-        PrimaryButton(onClick = navigateToAddressScreen) {
-            Text(text = "Continuar")
+        Text(
+            modifier = Modifier
+                .padding(vertical = paddingSmall)
+                .fillMaxWidth(),
+            text = screenState.headerTitle,
+            color = secondaryLight,
+            fontSize = fontSizeMedium
+        )
+        IsolationColumn {
+            OutlinedTextField(
+                value = inputState.value,
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = viewModel::onValueChanged,
+                label = { Text(screenState.textFieldLabel) },
+                isError = inputState.isStateInvalid,
+                supportingText = { ErrorTextField(inputValidation = inputState.validation) }
+            )
+        }
+        Row(Modifier.fillMaxHeight()) {
+            PrimaryButton(
+                Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Bottom),
+                onClick = {
+                    viewModel.onClickToContinue(goToBirthdayScreen = navigateToAddressScreen)
+                }
+            ) {
+                Text(text = stringResource(id = R.string.register_continue_button))
+            }
         }
     }
 }
---
+++ app/src/main/java/com/github/gustavobarbosab/androidcourse/ui/screen/register/screens/document/RegisterDocumentViewModel.kt
@@ -0,0 +1,9 @@
+package com.github.gustavobarbosab.androidcourse.ui.screen.register.screens.document
+
+import androidx.lifecycle.ViewModel
+import com.github.gustavobarbosab.androidcourse.ui.screen.register.data.RegisterFlowRepository
+
+class RegisterDocumentViewModel(
+    private val repository: RegisterFlowRepository
+) : ViewModel() {
+}
