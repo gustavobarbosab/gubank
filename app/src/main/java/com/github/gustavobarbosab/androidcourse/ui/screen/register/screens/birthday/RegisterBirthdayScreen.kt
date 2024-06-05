935f7883ada056c4e039270d5dfebeee40eb1f1b
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2024-06-05 12:23:38 -0300

    implementing new rules to select the birthday date

--- app/src/main/java/com/github/gustavobarbosab/androidcourse/ui/screen/register/screens/birthday/RegisterBirthdayScreen.kt
+++ app/src/main/java/com/github/gustavobarbosab/androidcourse/ui/screen/register/screens/birthday/RegisterBirthdayScreen.kt
@@ -2,34 +2,113 @@ package com.github.gustavobarbosab.androidcourse.ui.screen.register.screens.birt
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.github.gustavobarbosab.androidcourse.R
+import com.github.gustavobarbosab.androidcourse.ui.common.components.ErrorTextField
 import com.github.gustavobarbosab.androidcourse.ui.common.components.PrimaryButton
 import com.github.gustavobarbosab.androidcourse.ui.common.components.ToolbarIcon
+import com.github.gustavobarbosab.androidcourse.ui.common.size.fontSizeMedium
+import com.github.gustavobarbosab.androidcourse.ui.common.size.paddingSmall
+import com.github.gustavobarbosab.androidcourse.ui.common.theme.AndroidCourseTheme
+import com.github.gustavobarbosab.androidcourse.ui.common.theme.secondaryLight
 import com.github.gustavobarbosab.androidcourse.ui.screen.register.RegisterFlowViewModel
-import com.github.gustavobarbosab.androidcourse.ui.screen.register.common.extension.LaunchToolbar
+import com.github.gustavobarbosab.androidcourse.ui.screen.register.RegisterFlowViewModelFactory
+import com.github.gustavobarbosab.androidcourse.ui.screen.register.common.model.RegisterScreenState
+import com.github.gustavobarbosab.androidcourse.ui.screen.register.data.RegisterFlowRepositoryImpl
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegisterBirthdayScreen(
+    screenState: RegisterScreenState = RegisterScreenState(
+        stringResource(R.string.register_toolbar),
+        stringResource(R.string.register_name_header),
+        stringResource(R.string.register_name_hint)
+    ),
     viewModel: RegisterBirthdayViewModel,
     sharedViewModel: RegisterFlowViewModel,
     navigateToDocumentScreen: () -> Unit
 ) {
-    sharedViewModel.LaunchToolbar(
-        "Cadastro - Etapa 2 de 4",
-        ToolbarIcon.Back
+    LaunchedEffect(Unit) {
+        sharedViewModel.setupToolbar(
+            "Cadastro - Etapa 2 de 4",
+            ToolbarIcon.Back
+        )
+    }
+
+    val dateValidationState by viewModel.dateValidationState.collectAsState()
+
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = viewModel.preSelectedDate,
+        initialDisplayMode = DisplayMode.Picker,
+        selectableDates = object : SelectableDates {
+            override fun isSelectableDate(utcTimeMillis: Long): Boolean =
+                viewModel.isSelectableDate(utcTimeMillis)
+
+            override fun isSelectableYear(year: Int): Boolean =
+                viewModel.isSelectableYear(year)
+        }
     )
+
+    LaunchedEffect(datePickerState.selectedDateMillis) {
+        viewModel.onDateSelected(datePickerState.selectedDateMillis)
+    }
+
     Column(
         Modifier
-            .fillMaxSize()
             .background(Color.White)
+            .padding(paddingSmall)
+            .fillMaxSize()
     ) {
-        PrimaryButton(onClick = navigateToDocumentScreen) {
-            Text(text = "Continuar")
+        Text(
+            modifier = Modifier
+                .padding(vertical = paddingSmall)
+                .fillMaxWidth(),
+            text = screenState.headerTitle,
+            color = secondaryLight,
+            fontSize = fontSizeMedium
+        )
+        DatePicker(
+            state = datePickerState,
+            showModeToggle = false,
+            title = null,
+            headline = null,
+        )
+        ErrorTextField(inputValidation = dateValidationState)
+        Row(Modifier.fillMaxHeight()) {
+            PrimaryButton(
+                Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Bottom),
+                onClick = {
+                    viewModel.onClickToContinue(
+                        datePickerState.selectedDateMillis,
+                        goToNextScreen = navigateToDocumentScreen
+                    )
+                }
+            ) {
+                Text(text = stringResource(id = R.string.register_name_button))
+            }
         }
     }
 }
@@ -37,12 +116,22 @@ fun RegisterBirthdayScreen(
 @Preview(device = "id:Nexus 4")
 @Composable
 private fun RegisterBirthdayScreenPreview() {
-//    val navController = rememberNavController()
-//    val scopedViewModelStoreOwner = scopedViewModelStoreOwner()
-//    AndroidCourseTheme {
-//        RegisterBirthdayScreen(
-//            appNavigator = AppNavigatorImpl(navController),
-//            registerFlowViewModel = RegisterFlowViewModel.getInstance(scopedViewModelStoreOwner)
-//        )
-//    }
+    val factory = remember {
+        RegisterFlowViewModelFactory.provideFactory(RegisterFlowRepositoryImpl())
+    }
+    val screenState = remember {
+        RegisterScreenState(
+            "Novo cadastro - Etapa 2 de 4",
+            "Informe sua data de nascimento para prosseguirmos com seu cadastro.",
+            "Data de nascimento"
+        )
+    }
+    AndroidCourseTheme {
+        RegisterBirthdayScreen(
+            screenState,
+            viewModel(factory = factory),
+            viewModel(factory = factory),
+            {}
+        )
+    }
 }
